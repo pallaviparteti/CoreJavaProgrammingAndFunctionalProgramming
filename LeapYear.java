2202e9bce238715bb2ea47b0829f7c772123b41a
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-02-07 11:08:32 +0530

    Added LeapYear file

--- LeapYear.java
+++ LeapYear.java
@@ -2,18 +2,18 @@ package com.CoreJavaProgramming;
 
 public class LeapYear {
 
-public static void main ( String args[]) {
-	int year = 2020;
-	if(year % 400 == 0) {
-		System.out.println(year + " Is a leap year ");
-		if(year % 4 == 0 && year % 100 != 0) {
-			System.out.println( year + " is a leap year");
+	public static void main ( String args[]) {
+		int year = 2020;
+		if(year % 400 == 0) {
+			System.out.println(year + " Is a leap year ");
+			if(year % 4 == 0 && year % 100 != 0) {
+				System.out.println( year + " is a leap year");
+			}
+			else
+			{
+				System.out.println(year + "Is not a leap year" );
+			}
 		}
-		else
-		{
-			System.out.println(year + "Is not a leap year" );
-		}
+
 	}
-	
-}
 }
