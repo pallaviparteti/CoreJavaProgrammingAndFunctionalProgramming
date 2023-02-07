39bca53479307197607cde462e41f7fb7e5c8594
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-02-07 11:05:19 +0530

    Added FlipCoinPercentageOfHeadAndTail file

--- FlipCoinPercentageOfHeadAndTail.java
+++ FlipCoinPercentageOfHeadAndTail.java
@@ -4,20 +4,20 @@ public class FlipCoinPercentageOfHeadAndTail {
 	public static void main ( String args[]) {
 		int head = 0, tail =0;
 		for(int i =1; i<=100; i++) {
-		if(Math.random() < 0.5)
-		{
-			System.out.println("Head");
-			head++;
-		double percent = (head/10)*100;
-		System.out.println(percent);
-		double percentTail = (tail/10)*100;
-		System.out.println(percentTail);
+			if(Math.random() < 0.5)
+			{
+				System.out.println("Head");
+				head++;
+				double percent = (head/10)*100;
+				System.out.println(percent);
+				double percentTail = (tail/10)*100;
+				System.out.println(percentTail);
+			}
+			else {
+				tail++;
+			}
+
 		}
-		else {
-		  tail++;
-		}
-		
-	}
 
 	}
 }
